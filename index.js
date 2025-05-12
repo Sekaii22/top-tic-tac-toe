c40810e48277a21ccf9c29a1f357933ca415e628
Sekaii22 <shawn.ng22@gmail.com>
2025-05-13 06:10:58 +0800

    Add gameController logic and extract out not related functions from gameboard module.

--- index.js
+++ index.js
@@ -8,15 +8,11 @@ const gameboard = (function() {
     let board = [[null, null, null], 
                  [null, null, null], 
                  [null, null, null]];
-    const winStates = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                       [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                       [0, 4, 8], [2, 4, 6]];
-    let winningCombination = null;
 
     const checkValid = function(pos) {
         // return false if pos is not a number or not a valid position
-        // or the position has already been filled or someone has already won
-        if (!Number.isInteger(pos) || pos < 0 || pos > 8 || winningCombination !== null)
+        // or the position has already been filled or game has ended
+        if (!Number.isInteger(pos) || pos < 0 || pos > 8)
             return false;
 
         let row = Math.floor(pos / 3);
@@ -46,40 +42,19 @@ const gameboard = (function() {
         let row = Math.floor(pos / 3);
         let col = pos % 3;
         board[row][col] = symbol;
+        noOfPosFilled++;
 
         return true;
     };
-
-    const checkWin = function(moves) {
-        // 8 possible winning combination
-        let combinationCountBucket = [0, 0, 0, 0, 0, 0, 0, 0];
-
-        moves.forEach((pos) => {
-            winStates.forEach((combi, index) => {
-                if (combi.includes(pos))
-                    combinationCountBucket[index]++;
-            });
-        });
-
-        winIndex = combinationCountBucket.indexOf(3);
-        if (winIndex !== -1) {
-            winningCombination = winStates[winIndex];
-            return true
-        }
-
-        return false;
-    };
-
-    const getWinningCombination = () => winningCombination;
 
     const reset = function() {
         board = [[null, null, null], 
                  [null, null, null], 
                  [null, null, null]];
-        winningCombination = null;
+        noOfPosFilled = 0;
     };
 
-    return {show, fill, checkWin, getWinningCombination, reset};
+    return {show, fill, reset};
 })();
 
 function createPlayer(name, symbol) {
@@ -94,4 +69,106 @@ function createPlayer(name, symbol) {
     return {name, symbol, addMove, getMoves};
 }
 
+const gameController = (function() {
+    // private
+    let gameStarted = false;
+    let turnNum = 0;
+
+    let player1;
+    let player2;
+    // each bucket represents the number of position in player's moves 
+    // that are found in that particular winState
+    let player1CountBuckets = [0, 0, 0, 0, 0, 0, 0, 0];
+    let player2CountBuckets = [0, 0, 0, 0, 0, 0, 0, 0];
+
+    // 8 possible winning combination,
+    const winStates = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                       [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                       [0, 4, 8], [2, 4, 6]];
+    let winningCombination = null;
+
+    const checkWin = function(buckets) {
+        // if any bucket count reaches 3, it means a winning combination is satisfied
+        winIndex = buckets.indexOf(3);
+        if (winIndex !== -1) {
+            winningCombination = winStates[winIndex];
+            return true;
+        }
+
+        return false;
+    };
+
+    const update = function(buckets, pos) {
+        // update bucket
+        winStates.forEach((combi, index) => {
+            if (combi.includes(pos))
+                buckets[index]++;
+        });
+    }
+
+    // public
+    const gameSetup = function(p1, p2) {
+        gameStarted = true;
+        turnNum = 0;
+        winningCombination = null;
+        player1 = p1;
+        player2 = p2;
+        player1CountBuckets = [0, 0, 0, 0, 0, 0, 0, 0];
+        player2CountBuckets = [0, 0, 0, 0, 0, 0, 0, 0];
+        gameboard.reset();
+    };
+
+    const takeTurn = function(pos) {
+        if (gameStarted) {
+            // p1: even turns, p2 = odd turns
+            if (turnNum % 2 === 0) {
+                if (gameboard.fill(player1.symbol, pos)) {
+                    gameboard.show();
+                    player1.addMove(pos);
+                    update(player1CountBuckets, pos);
+                    turnNum++;
+
+                    // check for win
+                    // else check for draw
+                    if (checkWin(player1CountBuckets)) {
+                        gameStarted = false;
+                        // set off win event
+                        alert(`Player 1 (${player1.symbol}) won`);
+                        return;
+                    }
+                }
+            } 
+            else {
+                if (gameboard.fill(player2.symbol, pos)) {
+                    gameboard.show();
+                    player2.addMove(pos);
+                    update(player2CountBuckets, pos);
+                    turnNum++;
+
+                    // check for win
+                    // else check for draw
+                    if (checkWin(player2CountBuckets)) {
+                        gameStarted = false;
+                        // set off win event
+                        alert(`Player 2 (${player1.symbol}) won`);
+                        return;
+                    }
+                }
+            } 
+
+            if (turnNum >= 9) {
+                gameStarted = false;
+                // set off draw event
+                alert(`Draw`);
+            }
+        }
+    };
+
+    return { gameSetup, takeTurn };
+})();
+
 console.log(gameboard);
+let p1 = createPlayer("Bob", "x");
+let p2 = createPlayer("Tim", "o");
+gameController.gameSetup(p1, p2);
+console.log(gameController);
